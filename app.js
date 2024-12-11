f6769a32ff4c3648c798cbc109c6ca06197927b2
sumehra2004 <153430430+sumehra2004@users.noreply.github.com>
2024-12-11 14:33:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+  res.send("hello docker");
+});
+app.listen(3000,'0.0.0.0',()=>{
+  console.log("server is running on 3000");
+});
